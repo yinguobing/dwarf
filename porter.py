006d9d50396a653738bfadc9b419812980e7dc9c
yinguobing <yinguobing@gmail.com>
2021-06-27 22:30:53 +0800

    wrap the MQ messenger in the file observer

--- porter.py
+++ porter.py
@@ -12,6 +12,16 @@ from watchdog.observers import Observer
 
 class FolderEventHandler(FileSystemEventHandler):
 
+    def __init__(self, mq_address, queue):
+        super().__init__()
+
+        # Setup the post office.
+        self.messenger = pika.BlockingConnection(
+            pika.ConnectionParameters(mq_address))
+        self.channel = self.messenger.channel()
+        self.channel.queue_declare(queue=queue)
+        self.queue = queue
+
     def on_created(self, event):
         print(event.event_type, event.src_path)
         self.send_message(event.src_path)
@@ -23,41 +33,28 @@ class FolderEventHandler(FileSystemEventHandler):
         print(event.event_type, event.src_path)
 
     def send_message(self, src_path):
-        file_type = 'Directory' if os.path.isdir(src_path) else 'File'
-        print("{} created: {}".format(file_type, src_path))
-
-
+        if not os.path.isdir(src_path):
+            self.channel.basic_publish(exchange='', routing_key=self.queue,
+                                       body=src_path)
+            
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-
     # Load the configuration file.
     cfg_file = sys.argv[1] if len(sys.argv) > 1 else 'config.yml'
     with open("config.yml", 'r') as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-    # Setup the post office.
-    post_office = pika.BlockingConnection(
-        pika.ConnectionParameters(cfg['rabbitmq']['address']))
-    channel = post_office.channel()
-
-    queue_name = cfg['rabbitmq']['queue']
-    channel.queue_declare(queue=queue_name)
-
-    message = "Hello world!"
-    channel.basic_publish(exchange='', routing_key=queue_name, body=message)
-
-    post_office.close()
-
-    # Where is the barn to watch?
+    # Where is the barn to watch? Make sure the folder already existed.
     barn = cfg['porter']['barn']
+    assert os.path.exists(barn), "Target folder not found, please check."
 
+    # Setup the file observer.
     observer = Observer()
-    event_handler = FolderEventHandler()
+    event_handler = FolderEventHandler(mq_address=cfg['rabbitmq']['address'],
+                                       queue=cfg['rabbitmq']['queue'])
     observer.schedule(event_handler, barn, recursive=True)
     observer.start()
 
+    # Let it go.
     try:
         while observer.is_alive():
             observer.join(1)
