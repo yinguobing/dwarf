cb04b173f72af38656ddf95377697c1250041b40
yinguobing <yinguobing@gmail.com>
2021-06-27 16:28:59 +0800

    add YAML configure file for porter

--- porter.py
+++ porter.py
@@ -2,6 +2,7 @@
 
 import sys
 import logging
+import yaml
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
@@ -9,10 +10,18 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+
+    # Load the configuration file.
+    cfg_file = sys.argv[1] if len(sys.argv) > 1 else 'config.yml'
+    with open("config.yml", 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+    # Where is the barn to watch?
+    barn = cfg['porter']['barn']
+
     event_handler = LoggingEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, barn, recursive=True)
     observer.start()
     try:
         while observer.is_alive():
